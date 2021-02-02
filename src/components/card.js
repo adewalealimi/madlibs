9ac6456b0f25c11d3a93f98f5db1f9b87a743014
adewalealimi <alimiadewale68@gmail.com>
2021-02-02 05:20:46 -0800

    created Content component to generate story

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
+
 import Input from './input';
+import Content from './content';
+
 
 class Card extends Component {
 
@@ -69,6 +72,7 @@ class Card extends Component {
         {
           inputData.map(data => Input( (data), this.handleInputChange ))
         }
+        <Content data={this.state}/>
         </div>
       );
     }
