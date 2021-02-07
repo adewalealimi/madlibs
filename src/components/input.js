777613f4c96a024e3c5db75c1db5101002506008
adewalealimi <alimiadewale68@gmail.com>
2021-02-06 18:57:20 -0800

    put the content labels

--- src/components/input.js
+++ src/components/input.js
@@ -6,7 +6,8 @@ const Input = ({title, state, name}, onChange, index) => {
     return (
         <div key={index} className="input">
             <label className="input__number">{index + 1}</label>
-            <input name={name} value={state} onChange={onChange}/>
+            <label className={`input__number ${state == '' ? 'gray' : 'green'}`}>{index + 1}</label>
+            <input placeholder={title} name={name} value={state} onChange={onChange}/>
             <label className="input__title">{title}</label>
         </div>
     )
